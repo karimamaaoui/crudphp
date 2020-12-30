517ffed110402398b440469a6d4fd535309bbe1a
karimamaaoui <67017326+karimamaaoui@users.noreply.github.com>
2020-12-30 23:48:20 +0100

    Add files via upload

--- editerProd.php
+++ editerProd.php
@@ -1,32 +1,46 @@
 <?php
+session_start();
     require 'conx.php';
     if (!empty($_GET['id']))
     {
         $id=$_REQUEST['id'];
     }
     
-        $query=$pdo->query('SELECT id,nom FROM categories');
+        $query=$pdo->query('SELECT nom FROM categories');
         $categories=$query->fetchAll(PDO::FETCH_ASSOC);
-
+        
     if (!empty($_POST))
     {
-        $id=$_GET['id'];
-        $nom=$_POST['nom'];
-        $description=$_POST['description'];
-        $prix=$_POST['prix'];
-        $categorie=$_POST['categorie'];
+        if (isset($_POST['editer']))
+        {
+            $id=$_GET['id'];
+            $nom=$_POST['nom'];
+            $description=$_POST['description'];
+            $prix=$_POST['prix'];
+            $categorie=$_POST['categorie'];
+            $edit_images=$_FILES['fileimage']['name'];
+            var_dump($edit_images);
+            $tmp=$_FILES['fileimage']['tmp_name'];
+
+            
+            
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $sql1="UPDATE `produits` SET `nom`='$nom',`description`='$description',`prix`=$prix,`image`='$edit_images',`categories_id`=$categorie WHERE id=$id;";
+            $q = $pdo->prepare($sql1);
+            
+            if ($q)
+            {
+                move_uploaded_file( $_FILES['fileimage'] ['tmp_name'], "upload/".$_FILES['fileimage']['name']); 
+                
+            }
+        
+                $_SESSION['message']="mise  a jour  ";
+        
+            
+        //header("location: ListeProduit.php");
+        //exit();
     
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        
-        $sql1="UPDATE `produits` SET `nom`='$nom',`description`='$description',`prix`=$prix,`categories_id`=$categorie WHERE id=$id;";
-        
-        $q = $pdo->prepare($sql1);
-        $q->execute(array($nom,$description, $prix, $categorie, $id));
-       
-        
-        header("location: ListeProduit.php");
-        exit();
-    }
+        }}
     else
     {
         
@@ -49,5 +63,6 @@
 include "editerProd.phtml";
         
 ?>
+
 
 
